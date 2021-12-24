edf7bdcced1ccdf54e4003c7a41ba173611e64f9
dann9422 <dannsk82294@gmail.com>
2021-12-24 14:38:09 -0600

    cambios en la interfaz

--- Startup.cs
+++ Startup.cs
@@ -39,7 +39,7 @@ namespace turnos
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
+            if(env.IsProduction())
             {
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
